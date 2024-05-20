1b793485af1c56c893ee32ba7dba0972be37316d
Alexandra100layrenko <66309809+Alexandra100layrenko@users.noreply.github.com>
2024-05-20 16:27:23 +0100

    update all

--- src/main.js
+++ src/main.js
@@ -1,32 +1,24 @@
-import {
-    renderGallery,
-    showLoader,
-    hideLoader,
-    handleError,
-    showWarning
-} from './js/render-functions.js';
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
 
-import pixabayApi from './js/pixabay-api.js';
+document.querySelector('#app').innerHTML = `
+  <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="${viteLogo}" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
+      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
+    </a>
+    <h1>Hello Vite!</h1>
+    <div class="card">
+      <button id="counter" type="button"></button>
+    </div>
+    <p class="read-the-docs">
+      Click on the Vite logo to learn more
+    </p>
+  </div>
+`
 
-const searchForm = document.querySelector('.search-form');
-const searchInput = document.querySelector('.search-input');
-
-searchForm.addEventListener('submit', async (event) => {
-    event.preventDefault();
-
-    const query = searchInput.value.trim();
-    if (query === '') {
-        showWarning('Please enter a search query!');
-        return;
-    }
-
-    showLoader();
-    try {
-        const images = await pixabayApi.fetchImages(query);
-        renderGallery(images);
-    } catch (error) {
-        handleError(error);
-    } finally {
-        hideLoader();
-    }
-});
+setupCounter(document.querySelector('#counter'))
